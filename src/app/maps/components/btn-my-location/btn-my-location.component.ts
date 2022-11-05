a518c97646473531ffaa15580126656b217bf5ac
Leyder Quintero <lele.quinter@gmail.com>
2022-11-04 19:36:43 -0500

    Servicio para controlar el mapa

--- src/app/maps/components/btn-my-location/btn-my-location.component.ts
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
+import { MapService, PlacesService } from '../../services';
 
 @Component({
   selector: 'app-btn-my-location',
   templateUrl: './btn-my-location.component.html',
   styleUrls: ['./btn-my-location.component.css']
 })
-export class BtnMyLocationComponent  {
+export class BtnMyLocationComponent {
 
-  constructor() { }
+  constructor(
+    private placesService: PlacesService,
+    private mapService: MapService
+  ) { }
 
-  goToMylocation(){
-    console.log('ir al mi ubicacion');
+  goToMylocation() {
+
+    if (!this.placesService.isUserLocationReady) throw Error('No hay ubicacion de usuario');
+    if (!this.mapService.isMapReady) throw Error('No se ha inicializado el mapa');
+
+    this.mapService.flyTo( this.placesService.userLocation! );
+
   }
 
 }
--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { Map, Popup, Marker } from 'mapbox-gl';
-import { PlacesService } from '../../services/places.service';
+import { MapService, PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-view',
@@ -11,7 +11,7 @@ export class MapViewComponent implements AfterViewInit {
 
   @ViewChild('mapDiv') mapDivElement!: ElementRef;
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private mapService: MapService) { }
 
   ngAfterViewInit(): void {
 
@@ -34,6 +34,8 @@ export class MapViewComponent implements AfterViewInit {
       .setLngLat(this.placesService.userLocation)
       .setPopup(popup)
       .addTo(map);
+
+      this.mapService.setMap(map);
 
   }
 
