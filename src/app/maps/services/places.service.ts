b670322df39945679ec9ee9464b833ddc80e5847
Leyder Quintero <lele.quinter@gmail.com>
2022-11-05 13:27:42 -0500

    Ajustar el mapa para mostrar todos los marcadores encontrados

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -62,7 +62,7 @@ export class PlacesService {
         this.isLoadingPlaces = false;
         this.places = res.features;
 
-        this.mapService.createMarkersFromPlaces(this.places);
+        this.mapService.createMarkersFromPlaces(this.places, this.userLocation!);
       })
 
   }
